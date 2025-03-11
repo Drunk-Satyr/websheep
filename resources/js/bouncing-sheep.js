68b53bddc57c53a7240a916fea1a3a431b0da7fc
Caroline Oliver <me@carolineoliver.net>
2025-03-10 22:16:58 -0700

    added additional error checking and comments

--- resources/js/bouncing-sheep.js
+++ resources/js/bouncing-sheep.js
@@ -6,34 +6,39 @@ position = {x: 1, y: 1};
 direction = {x: 1, y: 1};
 speed = 3;
 
-function clamp() {
-    if (direction.x < -1) direction.x = -1;
-    if (direction.y < -1) direction.y = -1;
-    if (direction.x > 1) direction.x = 1;
-    if (direction.y > 1) direction.y = 1;
-}
-
 function handleMovement() {
     const screenHeight = document.body.clientHeight;
     const screenWidth = document.body.clientWidth;
 
+    // bounce along top or bottom
     if (position.y + sheepHeight >= screenHeight || position.y < 0) {
         direction.y *= -1;
     }
 
+    // bounce along left or right
     if (position.x + sheepWidth >= screenWidth || position.x < 0) {
         direction.x *= -1;
     }
 
-    clamp();
+    // push back in bounds if it gets caught outside
+    if (position.y + sheepHeight > screenHeight) position.y = screenHeight - 1;
+    if (position.x + sheepWidth > screenWidth) position.x = screenWidth - 1;
+    if (position.y < 0) position.y = 0;
+    if (position.x < 0) position.x = 0;
 
+    // clamps direction to [-1,1]
+    if (direction.x < -1) direction.x = -1;
+    if (direction.y < -1) direction.y = -1;
+    if (direction.x > 1) direction.x = 1;
+    if (direction.y > 1) direction.y = 1;
+
+    // calculates new position
     position.x += speed * direction.x;
     position.y += speed * direction.y;
 
+    // updates element's style to update position
     sheep.style.left = position.x + 'px';
     sheep.style.top = position.y + 'px';
-
-    console.log("direction x: " + direction.x + ", direction: y: " + direction.y + ".");
 }
 
 function update() {
